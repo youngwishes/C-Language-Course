023b262d0d3763d05b2113f1fcf1fbfc43a52ecc
Данил Федоров <mysc1@yandex.ru>
2025-11-10 21:22:33 +0300

    Configured vscode

--- module_2/lesson_3/lesson_3.c
+++ module_2/lesson_3/lesson_3.c
@@ -5,28 +5,28 @@
 int main(void)
 {
     printf("=== ПРОСТОЙ ВВОД/ВЫВОД СИМВОЛОВ ===\n");
-    
+
     // putchar() - вывод одного символа
     printf("putchar() выводит: ");
     putchar('A');           // выводим символ 'A'
     putchar('\n');          // выводим символ новой строки
     putchar('B');
     putchar('\n');
-    
+
     // getchar() - ввод одного символа
     printf("Введите один символ и нажмите Enter: ");
     int ch = getchar();     // читаем символ из потока ввода
     printf("Вы ввели: ");
     putchar(ch);            // выводим введенный символ
     putchar('\n');
-    
+
     printf("\n=== ЧТЕНИЕ НЕСКОЛЬКИХ СИМВОЛОВ ===\n");
-    
+
     printf("Введите три символа подряд (без пробелов): ");
     char c1 = getchar();
-    char c2 = getchar(); 
+    char c2 = getchar();
     char c3 = getchar();
-    
+
     printf("Вы ввели: ");
     putchar(c1);
     putchar('-');
